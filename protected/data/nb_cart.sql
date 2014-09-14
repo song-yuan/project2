bba0e82cdeed9bfa0d40d4bd94b80a700d342f2f
dys <daiyongsheng.good@163.com>
2014-09-14 18:46:14 +0800

    cart.sql

--- protected/data/nb_cart.sql
+++ protected/data/nb_cart.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2014-09-14 12:05:26
+-- Generation Time: 2014-09-14 12:44:45
 -- 服务器版本： 5.6.17
 -- PHP Version: 5.5.12
 
@@ -19,6 +19,22 @@ SET time_zone = "+00:00";
 --
 -- Database: `newdb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `nb_cart`
+--
+
+CREATE TABLE IF NOT EXISTS `nb_cart` (
+  `cart_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `product_id` int(10) unsigned NOT NULL,
+  `company_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `code` varchar(10) NOT NULL,
+  `product_num` int(10) unsigned NOT NULL,
+  `create_time` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`cart_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
