0247621f5e236f297a9c9bc7d937424b63d7e0be
dys <daiyongsheng.good@163.com>
2014-09-23 23:32:40 +0800

    cart order

--- protected/views/product/orderlist.php
+++ protected/views/product/orderlist.php
@@ -6,10 +6,12 @@
 	  <div class="seatnum"><?php echo $seatNum;?></div>
 	</div>
 	<div class="clear"></div>
+	<div class="orderup"><a href="<?php echo $this->createUrl('/product/cartList');?>"><div class="ordercart">已选产品</div></a><a href="<?php echo $this->createUrl('/product/orderList');?>"><div class="ordercart active">已下单产品</div></a></div>
+	<div class="clear"></div>
 	<div class="orderlist">
 	 <div class="order-cat">
 	  <div class="cat-left">订单总价:</div>
-	  <div class="cat-right">共<?php echo $totalPrice[0];?>元</div>
+	  <div class="cat-right">共<?php echo $totalPrice[0]?$totalPrice[0]:0;?>元</div>
 	 </div>
 	  <div class="clear"></div>
 	  <?php if($orderProducts):?>
