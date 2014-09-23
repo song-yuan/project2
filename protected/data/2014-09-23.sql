fc6702f1f6c7ce2e2d0d46c9dc482938d70c2c1e
yqs <yqs01221@example.com>
2014-09-23 23:32:50 +0800

    product

--- protected/data/2014-09-23.sql
+++ protected/data/2014-09-23.sql
@@ -1,2 +1,3 @@
 alter table nb_order add column `reality_total` decimal(12,2) unsigned NOT NULL DEFAULT '0.00',
 add column  `remark` text NOT NULL COMMENT '备注';
+ALTER TABLE  `nb_product` ADD  `status` TINYINT UNSIGNED NOT NULL DEFAULT  '0' COMMENT  '0正常,下架' AFTER  `create_time` ;
