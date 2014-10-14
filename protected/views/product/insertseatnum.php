c164fcd24501637b19909637ab55c6ac07669f35
dys <daiyongsheng.good@163.com>
2014-10-14 20:50:56 +0800

    seatenum

--- protected/views/product/insertseatnum.php
+++ protected/views/product/insertseatnum.php
@@ -4,8 +4,8 @@ Yii::app()->clientScript->registerCssFile('css/insertseatnum.css');
 ?>
 <form action="" method="post">
 <div class="form-group">
-  <div class="left-text">提示:请输入服务员给您的座次号才能点单！！</div>
-  <div class="right-ipt"><input type="text" class="inpt" name="seatnum" value="" maxlength="10" placeholder="请输入服务员提供的座次号" /></div>
+  <div class="left-text">请输入服务员给您的开台号才能点单！！</div>
+  <div class="right-ipt"><input type="text" class="inpt" name="seatnum" value="" maxlength="10" placeholder="开台号" /></div>
   <div class="error"><?php echo $error;?></div>
 <input type="hidden" name="referUrl" value="<?php echo $url;?>"/>
 </div>
